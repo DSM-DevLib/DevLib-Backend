1b729dfa57cc07898c2395c8411910c6ea320a2b
rlaisqls <rlaisqls@gmail.com>
2024-05-01 11:23:31 +0900

    chore: 기본 검색 수정

--- src/main/kotlin/kr/hs/dsm/devlib/global/feign/BookClient.kt
+++ src/main/kotlin/kr/hs/dsm/devlib/global/feign/BookClient.kt
@@ -5,11 +5,11 @@ import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 
-const val COMMON: String = "코드 아키텍처 JS"
-const val BACKEND: String = "스프링부트 SQL AWS node"
-const val FRONTEND: String = "Typescript vue 리액트"
-const val IOS: String = "Swift UIKit"
-const val ANDROID: String = "Kotlin Android"
+const val COMMON: String = "프로그래머 코딩테스트 네트워크"
+const val BACKEND: String = "스프링부트 AWS 백엔드"
+const val FRONTEND: String = "Typescript 리액트 웹"
+const val IOS: String = "Swift 앱"
+const val ANDROID: String = "Kotlin 안드로이드"
 val ALL = arrayOf(COMMON, BACKEND, FRONTEND, IOS, ANDROID).joinToString(" ")
 
 class FeignBooksResponse(
